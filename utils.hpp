2a12ec214ad28d39e167c8f8416908d05c8fbd1e
benroque <benjamin.m.roque@lawrence.edu>
2016-06-28 09:40:10 -0500

    Changes that we forgot

--- utils.hpp
+++ utils.hpp
@@ -6,47 +6,50 @@
 
 using namespace Eigen;
 
-/*
- * Cross product for Vector2d types
- */
-double jw_cross(Vector2d u, Vector2d v){
-	return u(0)*v(1)-u(1)*v(0);
+namespace{
+	/*
+ 	* Cross product for Vector2d types
+ 	*/
+	double jw_cross(Vector2d u, Vector2d v){
+		return u(0)*v(1)-u(1)*v(0);
+	}
+
+	/*
+ 	* Compare Vector2d objects
+ 	*/
+	int jw_comp_eq( Vector2d u, Vector2d v){
+		return (u(0)==v(0) && u(1)==v(1));
+	}
+
+	/*
+ 	* FromRefTri functor.
+ 	*
+ 	* This functor defines the transformation from the
+ 	* nu = (xi,eta) space of the reference triangle to
+ 	* the x,y space of the triangle given by the three
+ 	* points a,b,c.
+ 	*
+ 	* (0,0) should map to point a
+ 	* (1,0) should map to point b
+ 	* (0,1) should map to point c
+ 	*/
+	class FromRefTri{
+		Vector2d a, b, c;
+
+	public:
+		FromRefTri(Vector2d p0, Vector2d p1, Vector2d p2){
+			a = p0;
+			b = p1;
+			c = p2;
+		}
+		double jac(){
+			return jw_cross(a,b)+jw_cross(b,c)+jw_cross(c,a);
+		}
+		Vector2d operator()(Vector2d nu){
+			return (b-a)*nu(0) + (c-a)*nu(1) + a;
+		}
+	};
+
 }
-
-/*
- * Compare Vector2d objects
- */
-int jw_comp_eq( Vector2d u, Vector2d v){
-	return (u(0)==v(0) && u(1)==v(1));
-}
-
-/*
- * FromRefTri functor.
- *
- * This functor defines the transformation from the
- * nu = (xi,eta) space of the reference triangle to
- * the x,y space of the triangle given by the three
- * points a,b,c.
- *
- * (0,0) should map to point a
- * (1,0) should map to point b
- * (0,1) should map to point c
- */
-class FromRefTri{
-	Vector2d a, b, c;
-
-public:
-	FromRefTri(Vector2d p0, Vector2d p1, Vector2d p2){
-		a = p0;
-		b = p1;
-		c = p2;
-	}
-	double jac(){
-		return jw_cross(a,b)+jw_cross(b,c)+jw_cross(c,a);
-	}
-	Vector2d operator()(Vector2d nu){
-		return (b-a)*nu(0) + (c-a)*nu(1) + a;
-	}
-};
 
 #endif /*JW_FE_UTILS */
